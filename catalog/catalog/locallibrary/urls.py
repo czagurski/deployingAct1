d32a5a4bf5542cef9a4dc14f3963242f3c607ab6
czagurski <czagurski84@gmail.com>
2024-09-22 11:05:13 -0500

    another attempt
    trying once more

--- catalog/catalog/locallibrary/urls.py
+++ catalog/catalog/locallibrary/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf import settings
- 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
